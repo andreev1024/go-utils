9aada16e59beaabf41dcaa60fb81be2deabbac29
Andrey Zadorozhny <andreev1024@gmail.com>
2024-04-03 20:37:40 +0300

    add some funcs

--- utils.go
+++ utils.go
@@ -37,6 +37,44 @@ func IsIndexExist[T interface{}](index int, slice []T) bool {
 	return index >= 0 && index < len(slice)
 }
 
+// TODO generic
+func unique(input []string) []string {
+	set := make(map[string]struct{})
+	for i := range input {
+		set[input[i]] = struct{}{}
+	}
+
+	res := make([]string, 0, len(set))
+	for k := range set {
+		res = append(res, k)
+	}
+	return res
+}
+
+func reverseString(input string) string {
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func SetOf[V any, K comparable](v []V, fn func(V) K) map[K]V {
+	set := make(map[K]V, len(v))
+	for _, val := range v {
+		set[fn(val)] = val
+	}
+	return set
+}
+
+func Mapping[IN any, OUT any](input []IN, fn func(IN) OUT) []OUT {
+	res := make([]OUT, 0, len(input))
+	for _, val := range input {
+		res = append(res, fn(val))
+	}
+	return res
+}
+
 // func printTree(t *TreeNode) {
 // 	if t == nil {
 // 		fmt.Print("nil", ",")
